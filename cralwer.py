72d8a42dc95c179553ddc97c606f9fd614741eaa
Sonshi04 <arousalofneetysk6@gmail.com>
2023-05-15 04:20:41 +0900

    3rd commit

--- cralwer.py
+++ cralwer.py
@@ -28,7 +28,7 @@ for ip in ip_list:
   sleep(2)
   form_xpath = '/html/body/center/font/form/input[1]'
   click_button_xpath = '/html/body/center/font/form/input[3]'
-  
+
   form = driver.find_element(By.XPATH, form_xpath)
   form.clear()
   form.send_keys(ip)
@@ -72,7 +72,7 @@ for ip in ip_list:
 
   ws2.cell(row=index,column=1).value = ip
   if NG_count != 0:
-    ws2.cell(row=index,column=3).value = "\n".join(NG_sites)
+    ws2.cell(row=index,column=3).value = ", ".join(NG_sites)
     ws2.cell(row=index,column=2).value = "NG"
     ws2[f'B{index}'].font = openpyxl.styles.fonts.Font(color='FF0000')
   else:
@@ -80,19 +80,4 @@ for ip in ip_list:
     ws2[f'B{index}'].font = openpyxl.styles.fonts.Font(color='00793D')
   wb2.save("./res.xlsx")
   index += 1
-<<<<<<< HEAD
   print(trs_count)
-=======
-  print(trs_count)
-
-
-
-
-
-
-  
-
-
-
-
->>>>>>> origin/main
