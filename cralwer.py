6c087ac0e72ef8ec04b79dbf812bc079724f2049
Sonshi04 <arousalofneetysk6@gmail.com>
2023-05-17 02:58:26 +0900

    2サイト追加

--- cralwer.py
+++ cralwer.py
@@ -3,38 +3,47 @@ import openpyxl
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from time import sleep
 
-blacklistalert_url = "https://blacklistalert.org/"
 
 driver = webdriver.Chrome("./chromedriver.exe")
 wait = WebDriverWait(driver=driver, timeout=60)
-driver.get(blacklistalert_url)
 
 #読み込みexcelファイル
 wb = openpyxl.load_workbook("ip.xlsx")
 ws = wb.worksheets[0]
 #書き込みexcelファイル
-wb2 = openpyxl.load_workbook("res.xlsx")
+wb2 = openpyxl.load_workbook("result.xlsx")
 ws2 = wb2.worksheets[0]
 
 ip_list = []
-index = 1
+index = 2
 
 for col in ws["A"]:
   ip_list.append(col.value.splitlines()[0])
 
-for ip in ip_list:
-  sleep(2)
-  form_xpath = '/html/body/center/font/form/input[1]'
-  click_button_xpath = '/html/body/center/font/form/input[3]'
+blacklistalert_url = "https://blacklistalert.org/"
+blacklistalert_form_xpath = '/html/body/center/font/form/input[1]'
+click_button_xpath = '/html/body/center/font/form/input[3]'
 
-  form = driver.find_element(By.XPATH, form_xpath)
+spamcop_url = "https://www.spamcop.net/bl.shtml"
+spamcop_result_xpath = "//*[@id='content']/p"
+spamcop_form_selector = "#content > form input[name='ip']"
+spamcop_click_selector = "#content > form input[type=submit]"
+
+trendmicro_url = 'https://servicecentral.trendmicro.com/en-US/ers/ip-lookup/'
+trendmicro_result_selector = '.listedin.detail'
+trendmicro_form_selector = '#ip-form'
+trendmicro_click_selector = '#check-ip'
+
+for ip in ip_list:
+  ##各サイト巡回
+  driver.get(blacklistalert_url)
+  driver.implicitly_wait(1)
+  form = driver.find_element(By.XPATH, blacklistalert_form_xpath)
   form.clear()
   form.send_keys(ip)
   click_button = driver.find_element(By.XPATH,click_button_xpath)
   click_button.click()
-  print("clicked")
   wait.until(EC.text_to_be_present_in_element((By.TAG_NAME, 'body'),'sponsored'))
   ip_tables = driver.find_elements(By.TAG_NAME,'table')
   trs = ip_tables[0].find_elements(By.TAG_NAME, "tr")
@@ -46,7 +55,6 @@ for ip in ip_list:
     trs_count += 1
     site = tr.find_element(By.CLASS_NAME,'left').text
     status = tr.find_elements(By.TAG_NAME,"strong")
-    #Listedかどうか
     if len(status) == 0:
       status = "NG"
     else:
@@ -61,7 +69,6 @@ for ip in ip_list:
       trs_count += 1
       site = tr.find_element(By.CLASS_NAME,'left').text
       status = tr.find_elements(By.TAG_NAME,"strong")
-      #Listedかどうか
       if len(status) == 0:
         status = "NG"
       else:
@@ -69,8 +76,28 @@ for ip in ip_list:
       if status == "NG":
         NG_count += 1
         NG_sites.append(site)
+  print(f'ip:{ip}, blacklistalert_count:{trs_count}')
+  #spamcop
+  driver.get(spamcop_url)
+  driver.implicitly_wait(1)
+  spamcop_form = driver.find_element(By.CSS_SELECTOR, spamcop_form_selector)
+  spamcop_form.send_keys(ip)
+  spamcop_click_button = driver.find_element(By.CSS_SELECTOR,spamcop_click_selector)
+  spamcop_click_button.click()
+  spamcop_result = driver.find_elements(By.XPATH,spamcop_result_xpath)[0].text
+  #trendmicro
+  driver.get(trendmicro_url)
+  driver.implicitly_wait(1)
+  trendmicro_form = driver.find_element(By.CSS_SELECTOR, trendmicro_form_selector)
+  trendmicro_form.send_keys(ip)
+  trendmicro_click_button = driver.find_element(By.CSS_SELECTOR,trendmicro_click_selector)
+  trendmicro_click_button.click()
+  wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,trendmicro_result_selector),'List'))
+  trendmicro_result = driver.find_element(By.CSS_SELECTOR,trendmicro_result_selector).text
 
+  ##Excel書き込み
   ws2.cell(row=index,column=1).value = ip
+  #blacklistalert
   if NG_count != 0:
     ws2.cell(row=index,column=3).value = ", ".join(NG_sites)
     ws2.cell(row=index,column=2).value = "NG"
@@ -78,6 +105,21 @@ for ip in ip_list:
   else:
     ws2.cell(row=index,column=2).value = "OK"
     ws2[f'B{index}'].font = openpyxl.styles.fonts.Font(color='00793D')
-  wb2.save("./res.xlsx")
+  #spamcop
+  if 'not listed in' in spamcop_result:
+    ws2.cell(row=index,column=4).value = "OK"
+    ws2[f'D{index}'].font = openpyxl.styles.fonts.Font(color='00793D')
+  else:
+    ws2.cell(row=index,column=4).value = "NG"
+    ws2[f'D{index}'].font = openpyxl.styles.fonts.Font(color='FF0000')
+  #trendmicro
+  if 'None' in trendmicro_result:
+    ws2.cell(row=index,column=5).value = "OK"
+    ws2[f'E{index}'].font = openpyxl.styles.fonts.Font(color='00793D')
+  else:
+    ws2.cell(row=index,column=5).value = "NG"
+    ws2[f'E{index}'].font = openpyxl.styles.fonts.Font(color='FF0000')
+    ws2.cell(row=index,column=6).value = trendmicro_result
+  wb2.save("./result.xlsx")
   index += 1
-  print(trs_count)
+  
